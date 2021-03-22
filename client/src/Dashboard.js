8e38233ff0dafda3fdeadc2a0e239b84e0a1ef63
cristian.orozco <cristian.orozco@globant.com>
2021-03-22 10:35:27 -0500

    search track

--- client/src/Dashboard.js
+++ client/src/Dashboard.js
@@ -1,13 +1,66 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Container, Form } from 'react-bootstrap'
+import SpotifyWebApi from 'spotify-web-api-node'
+import { TrackSearchResult } from './TrackSearchResult'
+
 import { useAuth } from './useAuth'
+
+const spotifyApi = new SpotifyWebApi({
+  clientId: '1feee3008cdd43e1ae8c77b9fc525d8a',
+})
 
 export const Dashboard = ({ code }) => {
   const accessToken = useAuth(code)
+  const [search, setSearch] = useState('')
+  const [searchResults, setSearchResults] = useState([])
+
+  useEffect(() => {
+    if (!accessToken) return
+    spotifyApi.setAccessToken(accessToken)
+  }, [accessToken])
+
+  useEffect(() => {
+    if (!search) return setSearchResults([])
+    if (!accessToken) return
+
+    let cancel = false
+    spotifyApi.searchTracks(search).then((res) => {
+      if (cancel) return
+      setSearchResults(
+        res.body.tracks.items.map((track) => {
+          const smallestAlbumImage = track.album.images.reduce(
+            (smallest, image) => (image.height < smallest.height ? image : smallest),
+            track.album.images[0]
+          )
+
+          return {
+            artist: track.artists[0].name,
+            title: track.name,
+            uri: track.uri,
+            // albumUrl: track.albumUrl.images,
+            albumUrl: smallestAlbumImage.url, // after reducer method
+          }
+        })
+      )
+    })
+
+    return () => (cancel = true)
+  }, [search, accessToken])
 
   return (
-    <div>
-      <h1>Dashboard</h1>
-      <p>{code}</p>
-    </div>
+    <Container className='d-flex flex-column py-2' style={{ height: '100vh' }}>
+      <Form.Control
+        type='search'
+        placeholder='Search Songs/Artist'
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <div className='flex-grow-1 my-2' style={{ overflowY: 'auto' }}>
+        {searchResults.map((track) => (
+          <TrackSearchResult track={track} key={track.uri} />
+        ))}
+      </div>
+      <div>Bottom</div>
+    </Container>
   )
 }
--- client/src/useAuth.js
+++ client/src/useAuth.js
@@ -6,8 +6,6 @@ export const useAuth = (code) => {
   const [refreshToken, setRefreshToken] = useState()
   const [expriresIn, setExpriresIn] = useState()
 
-  console.log(refreshToken)
-
   useEffect(() => {
     axios
       .post('http://localhost:3001/login', {
